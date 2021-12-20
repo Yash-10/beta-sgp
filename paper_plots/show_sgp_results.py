87a87f1fe51f0f401a6c16c834d89748efeed590
Yash-10 <yashgondhalekar567@gmail.com>
2021-12-20 11:04:50 +0530

    Add SGP results script

---
+++ paper_plots/show_sgp_results.py
@@ -0,0 +1,32 @@
+import glob
+from astropy.io import fits
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+ORIGINAL = "SGP_original_images/*.fits"
+RECONSTRUCTED = "SGP_reconstructed_images/*.fits"
+
+for img in glob.glob(ORIGINAL):
+    arr = fits.getdata(img)
+
+    fig, ax = plt.subplots(1, 1, figsize=(5, 4))
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.07)
+    im = ax.imshow(arr, cmap='gray', origin="lower")
+    fig.colorbar(im, cax=cax, orientation='vertical')
+    ax.set_title("Original")
+    plt.savefig(img+".png", bbox_inches="tight", dpi=500)
+    #plt.close()
+
+for img in glob.glob(RECONSTRUCTED):
+    arr = fits.getdata(img)
+
+    fig, ax = plt.subplots(1, 1, figsize=(5, 4))
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.07)
+    im = ax.imshow(arr, cmap='gray', origin="lower")
+    fig.colorbar(im, cax=cax, orientation='vertical')
+    ax.set_title("Reconstructed")
+    plt.savefig(img+".png", bbox_inches="tight", dpi=500)
+    #plt.close()
