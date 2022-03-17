e53aa0eaa21a238721d82a896231ee853f89ea3a
Yash Gondhalekar <yashgondhalekar567@gmail.com>
2022-03-17 15:40:27 +0000

    Update

--- post_restoration_GC/applying_on_GC.py
+++ post_restoration_GC/applying_on_GC.py
@@ -1,6 +1,10 @@
 import numpy as np
+import sys
+import cv2
 import glob
 import matplotlib.pyplot as plt
+
+# import pandas as pd
 
 from itertools import repeat
 from timeit import default_timer as timer
@@ -26,12 +30,16 @@ mosaick = False  # Whether to mosaick the restored subdivisions.
 
 # Joblib setup
 from joblib import Memory
-cachedir = 'gc_cache/'
+cachedir = './gc_cache'
 memory = Memory(cachedir, verbose=0)
 
-def decide_star_cutout_size(data, bkg, nsigma=2.):
+def decide_star_cutout_size(data, bkg, subdiv_number, nsigma=2.):
     """
     data must be background-subtracted, But still need to pass bkg object.
+    subdiv_number: tuple of (x, y)
+        - Out of the 16 subdivisions, in which subdivision is the current star in?
+        - For subdivisions closer to center, offset must be very low - 1 or even 0.
+        - eg: (2, 2).
 
     Notes
     -----
@@ -70,7 +78,10 @@ def decide_star_cutout_size(data, bkg, nsigma=2.):
     y_extent = max(i) - min(i)
     x_extent = max(j) - min(j)
     approx_size = max(x_extent, y_extent)
-    _offset = 2
+    if subdiv_number[0] in [2, 3] and subdiv_number[1] in [2, 3]:  # Towards the center of cluster.
+        _offset = 1
+    else:
+        _offset = 3
 
     # # Calculate flux
     # # data must be background-subtracted.
@@ -112,12 +123,11 @@ def do_on_ys(image, ix, iy):
         try:  # For some reason, some subdivisions cannot be extracted (needs investigation).
             data = fits.getdata(cut_image)
         except:  # If "r" suffixed images cannot be extracted, used the non-resampled version.
-            # TODO: Note, using such subdivisions for mosaicking would not make sense.
+            mosaick = False  # Overwrite mosaick value since if except, then subdivisions are not actually resampled.
             _base_name = '_'.join((image.split('.')[0], str(ix), str(iy)))
             cut_image = '.'.join((_base_name, 'fits'))
             data = fits.getdata(cut_image)
             cut_coord_list = '.'.join((_base_name, 'coo'))
-            print(cut_coord_list)
 
         stars = np.loadtxt(cut_coord_list, skiprows=3, usecols=[0, 1])
         stars = apply_mask(stars, data)  # Exclude stars very close to the edge.
@@ -128,7 +138,7 @@ def do_on_ys(image, ix, iy):
         # t['y'] = stars[:, 1]
 
         for xc, yc in stars:
-            check_star_cutout = Cutout2D(data, position=(xc, yc), size=40)  # 40 is a safe size choice.
+            check_star_cutout = Cutout2D(data, position=(xc, yc), size=40, copy=True, mode='partial', fill_value=sys.float_info.epsilon)  # 40 is a safe size choice.
             # Estimate background on this check stamp
             d = np.ascontiguousarray(check_star_cutout.data)
             d = d.byteswap().newbyteorder()
@@ -137,19 +147,21 @@ def do_on_ys(image, ix, iy):
             bkg.subfrom(d)
 
             try:  # In very rare cases, no source is detected where finding size becomes difficult. If this happens, we simply move on to the next star.
-                size, other_mask, offset = decide_star_cutout_size(d, bkg, nsigma=2.)
+                size, other_mask, offset = decide_star_cutout_size(d, bkg, (ix, iy), nsigma=2.)
             except ValueError:
                 continue
 
-            # TODO: Update: Not really. Remove this todo. The stamp could still have multiple detections at this point. Do smth to mask them.
-            cutout = Cutout2D(data, position=(xc, yc), size=size)
+            # Interpolate to match shape of recon_img.
+            interpolated_bkg = cv2.resize(bkg.back(), dsize=(size, size), interpolation=cv2.INTER_CUBIC)
+
+            cutout = Cutout2D(data, position=(xc, yc), size=size, mode='partial', fill_value=bkg.globalback)
 
             ground_truth_star_stamp_name = '_'.join((_best_base_name, str(ix), str(iy))) + '.fits'
             ground_truth_star_stamp = fits.getdata(ground_truth_star_stamp_name)
             best_cut_image = Cutout2D(ground_truth_star_stamp, (xc, yc), size=size).data
 
             flux_before = calculate_flux(
-                cutout.data-bkg.globalback, offset, mask=other_mask, size=size
+                cutout.data, bkg.globalback, offset, size=size
             )
 
             if np.any(cutout.data > 1e10):
@@ -157,37 +169,28 @@ def do_on_ys(image, ix, iy):
 
             max_projs, gamma, beta, alpha_min, alpha_max, alpha, M_alpha, tau, M = DEFAULT_PARAMS
             try:
-                recon_img, rel_klds, rel_recon_errors, num_iters, extract_coord, execution_time, best_section, flux_before = sgp(
+                recon_img, rel_klds, rel_recon_errors, num_iters, extract_coord, execution_time, best_section = sgp(
                     cutout.data, psf, bkg.globalback, gamma=gamma, beta=beta, alpha_min=alpha_min, alpha_max=alpha_max,
                     alpha=alpha, M_alpha=M_alpha, tau=tau, M=M, proj_type=1, best_img=None, best_coord=(xc, yc), best_cutout=best_cut_image,
                     max_projs=max_projs, size=size, init_recon=2, stop_criterion=2, current_xy=(xc, yc), save=False,
-                    filename=image, verbose=True, clip_X_upp_bound=False, diapl=False, to_search_for_best_stamp=False, flux_stamp=flux_before
+                    filename=image, verbose=True, clip_X_upp_bound=False, diapl=False, to_search_for_best_stamp=False, offset=offset
                 )
             except ValueError:
                 continue
 
-            # fig, ax = plt.subplots(1, 2)
-            # ax[0].imshow(cutout.data)
-            # ax[1].imshow(recon_img)
-            # plt.show()
+            recon_bkg = Background(recon_img, bw=8, bh=8, fw=3, fh=3)  # 8 = 40 / 5
+            recon_bkg.subfrom(recon_img)
+            recon_img += interpolated_bkg
 
-            ##recon_bkg = Background(recon_img, bw=8, bh=8, fw=3, fh=3)  # 8 = 40 / 5
-            ##recon_bkg.subfrom(recon_img)
-            ##_, recon_other_mask = decide_star_cutout_size(recon_img, recon_bkg, nsigma=2.)
-            ##flux_after = calculate_flux(recon_img, mask=None, size=size, nsigma=2.)
+            flux_after = calculate_flux(recon_img, interpolated_bkg, offset, size=size)
 
-            # Calculating restored star's flux cannot be calculated as of now since we do not have a good bkg estimate for the restored star
             print(f'Flux before: {flux_before}')
+            print(f'Flux after: {flux_after}')
 
-            # TODO: Can I add a varying bkg map still? - would be better...
-            cutout.data[...] = recon_img + bkg.globalback  # Trick to modify `cutout.data` inplace.
+            cutout.data[...] = recon_img  # Trick to modify `cutout.data` inplace.
 
         restored_cutout_filename = '_'.join(('restored', cut_image))
-        fits.writeto(restored_cutout_filename, data)
-
-def worker(args_batch):
-    image, ix, iy = args_batch
-    do_on_ys(image, ix, iy)
+        fits.writeto(restored_cutout_filename, data, overwrite=True)
 
 if __name__ == "__main__":
     with open("candidate_defect_images.txt", "r") as f:
@@ -196,28 +199,19 @@ if __name__ == "__main__":
     _best_base_name = 'ccfbtf170075' + 'r'
     best_cut_image = '.'.join((_best_base_name, 'fits'))
     best_cut_coord_list = '.'.join((_best_base_name, 'coo'))
-
-    # Calculate mean PSF (mean of PSFs over a single frame) as a prior for the SGP method.
-    psfs = sorted(glob.glob("/home/yash/DIAPL/work/_PSF_BINS/psf_cc*.fits"))
-    mean_psfs = []
-    for i in range(0, len(psfs), 4):
-        data_psfs = [fits.getdata(psfs[n]) for n in range(i, i+4)]
-        mean_psf = np.mean(data_psfs, axis=0)
-        mean_psfs.append(mean_psf)
+    
+    fluxes = []
+    count = 0
 
     start = timer()
-    for image, psf in zip(sorted(defect_images), mean_psfs):
+    for image in sorted(defect_images)[15:30]:
+        psfs = glob.glob(f"_PSF_BINS/psf_{image.split('.')[0]}_*_*.fits")
+        psf = np.mean([fits.getdata(p) for p in psfs], axis=0)
+
         if image not in defect_images:
             continue
         for ix in range(1, 5):
-            arguments = [[image, ix, 1], [image, ix, 2], [image, ix, 3], [image, ix, 4]]
-            with Parallel(n_jobs=4, verbose=10) as parallel:
-                funcs = repeat(do_on_ys, 4)  # functools.partial seems not pickle-able
-                args_batches = np.array_split(arguments, 4, axis=0)
-                print(*args_batches)
-                jobs = args_batches
-                parallel(delayed(worker)(*job) for job in jobs)
-            sleep(1)
+            Parallel(n_jobs=2, verbose=10)(delayed(do_on_ys)(image, ix, iy) for iy in range(1, 5))
 
         # Mosaick
         if mosaick:
@@ -227,6 +221,6 @@ if __name__ == "__main__":
             )
             # Save restored GC image
             fits.writeto(f'mosaicked_{image}', arr)
-        break
+
     end = timer() - start
     print(f"Time taken = {end}s")
