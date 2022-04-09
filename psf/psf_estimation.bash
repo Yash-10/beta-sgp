ea0fe03e4ba6eb4d8cc349c6b6076dab9d5d77d7
Yash Gondhalekar <yashgondhalekar567@gmail.com>
2022-04-09 06:07:21 +0100

    Update bash scripts

--- psf/psf_estimation.bash
+++ psf/psf_estimation.bash
@@ -4,7 +4,7 @@
 # This script is a modified version of the "getpsf" routines from the original DIAPL software.
 
 #--------------- PARAMETERS TO EDIT ------------------
-ERR_PFX="\n\tERROR! psf_estm.bash: "
+ERR_PFX="\n\tERROR! psf_estimation.bash: "
 INSTRUMENT="instrument.par"
 FITS=`awk '($1=="FITS") {print $3}' ${INSTRUMENT}`
 MASK="m"
@@ -15,105 +15,129 @@ NY=`awk '($1=="NY") {print $3}' ${INSTRUMENT}`
 MARG=`awk '($1=="MARG") {print $3}' ${INSTRUMENT}`
 #-----------------------------------------------------
 
+if ! [ -r diapl_setup.bash ]
+then
+  echo -e ${ERR_PFX}"cannot read 'diapl_setup.bash'\n"
+  exit 1
+fi
+source diapl_setup.bash
+
+(( nxw=NX+2*MARG ))
+(( nyw=NY+2*MARG ))
+
+(( xload=(NX0+NX-1)/NX ))
+(( yload=(NY0+NY-1)/NY ))
+
 echo
 echo "Calculating PSF for all images"
 echo "------------------------------"
 echo ""
 
-for im in `cat ${IMAGES}`;
+for im in `cat ${IMAGES}`
 do
-	name=${im%.${FITS}}
+	for iy in `seq  1 ${yload}`
+	do
+		for ix in `seq 1 ${xload}`
+		do
+			prefix="${ix}_${iy}"
+			name=${im%.${FITS}}
 
-	coord_file="${name}.coo"
-	psf_file="psf${name}.bin"
+			psf_file="psf${name}_${prefix}.bin"
 
-	if [ -f "$coord_file" ] ; then
-    		rm "$coord_file"
-		echo "removed existing file '${coord_file}'"
-	fi
+			if [ -f "$coord_file" ] ; then
+					rm "$coord_file"
+				echo "removed existing file '${coord_file}'"
+			fi
 
-	if [ -f "$psf_file" ] ; then
-                rm "$psf_file"
-                echo "removed existing file '${psf_file}'"
-        fi
+			if [ -f "$psf_file" ] ; then
+					rm "$psf_file"
+					echo "removed existing file '${psf_file}'"
+			fi
 
-	if [ -f "${name}c.${FITS}" ]; then
-		rm "${name}c.${FITS}"
-		echo "removed existing intermediate cutted images"
-	fi
+			if [ -f "${name}c.${FITS}" ]; then
+				rm "${name}c.${FITS}"
+				echo "removed existing intermediate cutted images"
+			fi
 
-	if [ -f "${name}mc.${FITS}" ]; then
-		rm "${name}mc.${FITS}"
-		echo "removed existing intermediate cutted mask images"
-	fi
+			if [ -f "${name}mc.${FITS}" ]; then
+				rm "${name}mc.${FITS}"
+				echo "removed existing intermediate cutted mask images"
+			fi
 
-	echo "${im}"
-	echo "--------------------"
-	
-	(( xl=1+MARG ))
-	(( xu=NX+MARG ))
-        (( yl=1+MARG ))
-        (( yu=NY+MARG ))
+			echo "${im}"
+			echo "--------------------"
+			
+			(( xl=1+MARG ))
+			(( xu=NX+MARG ))
+			(( yl=1+MARG ))
+			(( yu=NY+MARG ))
 
-	fim=${name}c.${FITS}
-	cim=${name}c.coo
-	imask=${name}${MASK}.${FITS}
-        cmask=${name}${MASK}c.${FITS}
-        
-	echo cutfitsim ${im} ${fim} ${xl} ${xu} ${yl} ${yu}
-        ${BIN}/cutfitsim ${im} ${fim} ${xl} ${xu} ${yl} ${yu}
-    	if [ $? -ne 0 ]
-    	then
-      	  echo -e ${ERR_PFX}"cutfitsim failed - aborting now\n"
-      	  exit 24
-    	fi
+			fim=${name}c${prefix}.${FITS}
+			cim=${name}c${prefix}.coo
+			imask=${name}${MASK}.${FITS}
+			cmask=${name}${MASK}c${prefix}.${FITS}
 
-    	echo cutfitsim ${imask} ${cmask} ${xl} ${xu} ${yl} ${yu}
-    	${BIN}/cutfitsim ${imask} ${cmask} ${xl} ${xu} ${yl} ${yu}
-    	if [ $? -ne 0 ]
-    	then
-      	  echo -e ${ERR_PFX}"cutfitsim failed - aborting now\n"
-          exit 25
-    	fi
-	
-	echo fwhmm fwhmm.par ${fim}
-	arg=(`${BIN}/fwhmm fwhmm.par ${fim}`);
-    	echo ${arg[*]}
+			echo cutfitsim ${im} ${fim} ${xl} ${xu} ${yl} ${yu}
+			${BIN}/cutfitsim ${im} ${fim} ${xl} ${xu} ${yl} ${yu}
+			if [ $? -ne 0 ]
+			then
+			echo -e ${ERR_PFX}"cutfitsim failed - aborting now\n"
+			exit 24
+			fi
 
-    	sky=${arg[2]}
-    	fwhm=${arg[3]}
-	
-	echo "sfind"
-	echo "-----"
+			echo cutfitsim ${imask} ${cmask} ${xl} ${xu} ${yl} ${yu}
+			${BIN}/cutfitsim ${imask} ${cmask} ${xl} ${xu} ${yl} ${yu}
+			if [ $? -ne 0 ]
+			then
+			echo -e ${ERR_PFX}"cutfitsim failed - aborting now\n"
+			exit 25
+			fi
 
-	echo sfind sfind.par ${INSTRUMENT} ${fim} ${cmask} ${sky} ${fwhm} ${cim}
-        ${BIN}/sfind sfind.par ${INSTRUMENT} ${fim} ${cmask} ${sky} ${fwhm} ${cim}
-        if [ $? -ne 0 ]
-        then
-          echo -e ${ERR_PFX}"sfind failed - aborting now\n"
-          exit 13
-        fi
+			echo fwhmm fwhmm.par ${fim}
+			arg=(`${BIN}/fwhmm fwhmm.par ${fim}`);
+				echo ${arg[*]}
 
-	echo "CALCULATE THE PSF COEFFICIENTS ON A SINGLE WHOLE FRAME"
-	echo ""
+				sky=${arg[2]}
+				fwhm=${arg[3]}
 
-	echo "getpsf"
-	echo "------"
-	
-	echo -n getpsf getpsf.par ${INSTRUMENT} ${fim} ${cmask} ${cim}
-    	echo " "psf_${name}.bin
-    	${BIN}/getpsf getpsf.par ${INSTRUMENT} ${fim} ${cmask} ${cim} psf_${name}.bin
-	
-	if [ $? -ne 0 ]; then continue; fi  # Go to next image if PSF calculation fails for current image.
-	
-	echo "removing unecessary files"
-	echo "-------------------------"
-        rm -f ${cim}
-	rm -f ${fim}
-	rm -f ${cmask}
+			echo "sfind"
+			echo "-----"
 
-	echo ""	
+			echo sfind sfind.par ${INSTRUMENT} ${fim} ${cmask} ${sky} ${fwhm} ${cim}
+				${BIN}/sfind sfind.par ${INSTRUMENT} ${fim} ${cmask} ${sky} ${fwhm} ${cim}
+				if [ $? -ne 0 ]
+				then
+				echo -e ${ERR_PFX}"sfind failed - aborting now\n"
+				exit 13
+				fi
+
+			echo "CALCULATE THE PSF COEFFICIENTS ON A SUB-FRAME"
+			echo ""
+
+			echo "getpsf"
+			echo "------"
+
+			echo -n getpsf getpsf.par ${INSTRUMENT} ${fim} ${cmask} ${cim}
+				echo " "psf_${name}_${prefix}.bin
+				${BIN}/getpsf getpsf.par ${INSTRUMENT} ${fim} ${cmask} ${cim} ${psf_file}
+				if [ $? -ne 0 ]
+				then
+					echo -e ${ERR_PFX}"getpsf failed for ${fim}"${suffix}"\n"
+					exit 26
+				fi
+
+			# if [ $? -ne 0 ]; then continue; fi  # Go to next image if PSF calculation fails for current image.
+
+			echo "removing unecessary files"
+			echo "-------------------------"
+
+			echo ""	
+		done
+	done
 done
 
-echo "DONE!"
+echo
+date
+echo "DONE"
+
 exit 0
